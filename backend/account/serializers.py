f5eff2b6eb496cf19aa8c384dcc7d8c25dd30c25
Tetiana Kozakova <kleotan901@gmail.com>
2024-08-06 18:39:23 +0300

    Add description to endpoints in swagger documentation

--- backend/account/serializers.py
+++ backend/account/serializers.py
@@ -36,3 +36,14 @@ class UserListSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
         fields = ("id", "email", "first_name", "last_name", "avatar_url", "bio")
+
+
+class UserDetailSerializer(UserListSerializer):
+    def get_full_name(self, obj):
+        return f"{obj.first_name} {obj.last_name}"
+
+    full_name = serializers.SerializerMethodField()
+
+    class Meta:
+        model = get_user_model()
+        fields = ("id", "email", "full_name", "avatar_url", "bio")
--- backend/account/views.py
+++ backend/account/views.py
@@ -1,10 +1,9 @@
-from drf_spectacular.types import OpenApiTypes
 from drf_spectacular.utils import extend_schema, OpenApiParameter
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
-from account.serializers import UserListSerializer, UserSerializer
+from account.serializers import UserSerializer, UserDetailSerializer, UserListSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
@@ -12,13 +11,37 @@ class CreateUserView(generics.CreateAPIView):
 
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
-    serializer_class = UserListSerializer
+    serializer_class = UserDetailSerializer
     authentication_classes = (JWTAuthentication,)
     permission_classes = (IsAuthenticated,)
 
     def get_object(self):
         return self.request.user
 
-    @extend_schema()
-    def retrieve(self, request, *args, **kwargs):
+    def get_serializer_class(self):
+        if self.request.method == 'GET':
+            return UserDetailSerializer
+        return UserSerializer
+
+    @extend_schema(
+        description="Retrieve the authenticated user's details.",
+        responses={200: UserDetailSerializer}
+    )
+    def get(self, request, *args, **kwargs):
         return super().retrieve(request, *args, **kwargs)
+
+    @extend_schema(
+        description="Update the authenticated user's details and/or change the password.",
+        request=UserSerializer,
+        responses={200: UserSerializer}
+    )
+    def put(self, request, *args, **kwargs):
+        return super().update(request, *args, **kwargs)
+
+    @extend_schema(
+        description="Partially update the authenticated user's details.",
+        request=UserListSerializer,
+        responses={200: UserListSerializer}
+    )
+    def patch(self, request, *args, **kwargs):
+        return super().partial_update(request, *args, **kwargs)
