0f2645be7ce51b5b9a3ae10a144327ca41d10bdb
Tetiana Kozakova <kleotan901@gmail.com>
2024-08-06 12:56:26 +0300

    Create views to handle API requests for User model

--- backend/account/views.py
+++ backend/account/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
-# Create your views here.
+from account.serializers import UserListSerializer, UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserListSerializer
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user
