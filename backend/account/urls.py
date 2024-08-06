bbc03b45cc23e6a2511c630b2128f889b3fff319
Tetiana Kozakova <kleotan901@gmail.com>
2024-08-06 12:54:52 +0300

    Add URL routing for the account app's endpoints

---
+++ backend/account/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView, TokenVerifyView
+)
+
+from account.views import CreateUserView, ManageUserView
+
+app_name = "account"
+
+urlpatterns = [
+    path("register/", CreateUserView.as_view(), name="create"),
+    path("token/", TokenObtainPairView.as_view(), name="token-obtain-pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token-refresh"),
+    path("token/verify/", TokenVerifyView.as_view(), name="token-verify"),
+    path("me/", ManageUserView.as_view(), name="manage"),
+]
