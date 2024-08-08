a2a5da319f74718ec566cd33d988112230f05d95
Tetiana Kozakova <kleotan901@gmail.com>
2024-08-08 21:58:19 +0300

    Add email confirmation routes and implement viewset for handling email verification

--- backend/account/urls.py
+++ backend/account/urls.py
@@ -1,13 +1,26 @@
 from django.urls import path
-from rest_framework_simplejwt.views import (TokenObtainPairView,
-                                            TokenRefreshView, TokenVerifyView)
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
-from account.views import CreateUserView, ManageUserView
+from account.views import CreateUserView, ManageUserView, SendEmailConfirmationView
 
 app_name = "account"
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
+    path(
+        "email-confirmation/",
+        SendEmailConfirmationView.as_view({"post": "post"}),
+        name="email-confirmation",
+    ),
+    path(
+        "email-verification/",
+        SendEmailConfirmationView.as_view({"post": "email_verification"}),
+        name="email-verification",
+    ),
     path("login/", TokenObtainPairView.as_view(), name="token-obtain-pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token-refresh"),
     path("token/verify/", TokenVerifyView.as_view(), name="token-verify"),
--- backend/account/views.py
+++ backend/account/views.py
@@ -1,10 +1,12 @@
-from drf_spectacular.utils import OpenApiParameter, extend_schema
-from rest_framework import generics
+from drf_spectacular.utils import extend_schema, OpenApiParameter
+from rest_framework import generics, status, viewsets
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
-from account.serializers import (UserDetailSerializer, UserListSerializer,
-                                 UserSerializer)
+from account.models import EmailConfirmationToken
+from account.serializers import UserDetailSerializer, UserListSerializer, UserSerializer
+from account.tasks import send_email
 
 
 class CreateUserView(generics.CreateAPIView):
@@ -46,3 +48,64 @@ class ManageUserView(generics.RetrieveUpdateAPIView):
     )
     def patch(self, request, *args, **kwargs):
         return super().partial_update(request, *args, **kwargs)
+
+
+class SendEmailConfirmationView(viewsets.ViewSet):
+    permission_classes = (IsAuthenticated,)
+
+    @extend_schema(
+        description="""
+        Sends an email confirmation link to the user's email address if the email is not verified.
+        """
+    )
+    def post(self, request, format=None):
+        user = request.user
+        if not user.email_is_verified:
+            token = EmailConfirmationToken.objects.create(user=user)
+            send_email.delay(user.email, token.id, user.id)
+            return Response(
+                {"message": "The activation link has been sent to your email!"},
+                status.HTTP_201_CREATED,
+            )
+        return Response(
+            {"message": "This email has already been verified"}, status.HTTP_201_CREATED
+        )
+
+    @extend_schema(
+        description="""
+        The activation link. Verifies the user's email using a token and user_id provided in the request.
+        """,
+        parameters=[
+            OpenApiParameter(
+                name="token_id",
+                description="token_id parameter",
+                required=True,
+                type=str,
+            ),
+            OpenApiParameter(
+                name="user_id",
+                description="user_id parameter",
+                required=True,
+                type=str,
+            ),
+        ],
+    )
+    def email_verification(self, request):
+        token_id = request.GET.get("token_id")
+        try:
+            token = EmailConfirmationToken.objects.get(id=token_id)
+            user = token.user
+            if user.email_is_verified:
+                return Response(
+                    {"message": "This email has already been verified"},
+                    status.HTTP_200_OK,
+                )
+            user.email_is_verified = True
+            user.save()
+            return Response(
+                {"message": "Email has been successfully verified!"}, status.HTTP_200_OK
+            )
+        except EmailConfirmationToken.DoesNotExist:
+            return Response(
+                {"message": "Email not confirm"}, status.HTTP_400_BAD_REQUEST
+            )
