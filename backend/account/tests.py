b6c63a9ac67ce2bb889323d64aab0853c3c350a0
Tetiana Kozakova <kleotan901@gmail.com>
2024-08-09 14:41:38 +0300

    Updated tests.py

--- backend/account/tests.py
+++ backend/account/tests.py
@@ -1,14 +1,18 @@
+from unittest.mock import patch
+
+from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 from rest_framework import status
 from rest_framework.reverse import reverse
 from rest_framework.test import APIClient
 
-from account.models import image_file_path
-from account.serializers import UserDetailSerializer
+from account.models import image_file_path, EmailConfirmationToken
+from account.serializers import UserDetailSerializer, UserSerializer
+from account.tasks import send_email
 
 
-class AccountCreateUserTestCase(TestCase):
+class AccountNotAuthenticatedUserTestCase(TestCase):
     def setUp(self):
         self.client = APIClient()
 
@@ -29,6 +33,20 @@ class AccountCreateUserTestCase(TestCase):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response.data["email"][0], "This field may not be blank.")
 
+    @patch("account.tasks.send_email.delay")
+    def test_email_activation_link_send_when_profile_created(self, mock_send_email):
+        payload = {"email": "emailtest@mail.com", "password": "testpassword"}
+        url = reverse("account:create")
+        response = self.client.post(url, payload)
+
+        user = get_user_model().objects.get(email=payload["email"])
+        token = user.tokens.all()[0]
+
+        mock_send_email.assert_called_once_with(user.email, token.id, user.id)
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(mock_send_email.called, 1)
+
 
 class AccountAuthenticatedUserTestCase(TestCase):
     def setUp(self):
@@ -38,6 +56,7 @@ class AccountAuthenticatedUserTestCase(TestCase):
             password="testpassword",
             first_name="John",
             last_name="Smith",
+            email_is_verified=False,
         )
         self.client.force_authenticate(user=user)
 
@@ -72,3 +91,28 @@ class AccountAuthenticatedUserTestCase(TestCase):
         self.assertEqual(
             serializer.data["full_name"], f"{user.first_name} {user.last_name}"
         )
+
+    def test_email_verification(self):
+        user = get_user_model().objects.get(email="user@test.com")
+        token = EmailConfirmationToken.objects.create(user=user)
+
+        url = reverse("account:email-verification")
+        activation_link = url + f"?token_id={token.id}&user_id={user.id}"
+        result = self.client.get(activation_link)
+
+        get_activated_user = get_user_model().objects.get(email="user@test.com")
+
+        self.assertEqual(result.status_code, status.HTTP_200_OK)
+        self.assertTrue(get_activated_user.email_is_verified)
+
+    def test_email_already_verified(self):
+        user = get_user_model().objects.get(email="user@test.com")
+        user.email_is_verified = True
+        token = EmailConfirmationToken.objects.create(user=user)
+        url = reverse("account:email-verification")
+        activation_link = url + f"?token_id={token.id}&user_id={user.id}"
+
+        result = self.client.get(activation_link)
+
+        self.assertEqual(result.status_code, status.HTTP_200_OK)
+        self.assertTrue(user.email_is_verified)
