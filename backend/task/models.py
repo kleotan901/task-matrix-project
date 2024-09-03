f1c06c2a3445a8aa3008b53ba141d926f1b8987a
Tetiana Kozakova <kleotan901@gmail.com>
2024-09-03 15:43:31 +0300

    Added validate_date_fields method for task app and renamed fields in the models.py

--- backend/task/models.py
+++ backend/task/models.py
@@ -1,30 +1,16 @@
 from django.conf import settings
 from django.db import models
-from rest_framework.reverse import reverse
+from rest_framework.exceptions import ValidationError
 
-
-class Tag(models.Model):
-    name = models.CharField(max_length=64, default=None)
-    user = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        related_name="tags",
-        on_delete=models.CASCADE,
-        null=True,
-    )
-
-    class Meta:
-        unique_together = ("name", "user")
-
-    def __str__(self):
-        return self.name
+from project.models import Project
 
 
 class Task(models.Model):
     PRIORITY_CHOICES = [
-        ("Urgent and important", "Do First"),
-        ("Important but Not Urgent", "Schedule"),
-        ("Urgent but Not Important", "Delegate"),
-        ("Neither Urgent nor Important", "Eliminate"),
+        ("Важливо і терміново", "Важливо і терміново"),
+        ("Важливо, але не терміново", "Важливо, але не терміново"),
+        ("Не важливо, але терміново", "Не важливо, але терміново"),
+        ("Не важливо і не терміново", "Не важливо і не терміново"),
     ]
 
     STATUS_CHOICES = [
@@ -33,23 +19,42 @@ class Task(models.Model):
         ("overdue", "overdue"),
     ]
     priority = models.CharField(max_length=50, choices=PRIORITY_CHOICES)
-    status = models.CharField(max_length=50, choices=STATUS_CHOICES)
+    status = models.CharField(
+        max_length=50, choices=STATUS_CHOICES, blank=True, default=None, null=True
+    )
 
     title = models.CharField(max_length=255)
+    project = models.ForeignKey(
+        Project,
+        related_name="tasks",
+        on_delete=models.SET_NULL,
+        null=True,
+    )
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
-        on_delete=models.SET_NULL,
+        on_delete=models.CASCADE,
         null=True,
         blank=True,
         related_name="created_tasks",
     )
-    description = models.TextField(max_length=500, blank=True)
+    comments = models.TextField(max_length=500, blank=True)
     start_date = models.DateTimeField(blank=True, null=True)
-    deadline = models.DateTimeField(blank=True, null=True)
     finish_date = models.DateTimeField(blank=True, null=True)
     is_completed = models.BooleanField(default=False)
-    assignees = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="tasks")
-    tags = models.ManyToManyField(Tag, related_name="tasks", blank=True)
+    assignees = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name="tasks", blank=True
+    )
 
     def __str__(self):
-        return f"{self.title} {self.deadline}({self.priority})"
+        return f"{self.title} - {self.finish_date}({self.priority})"
+
+    @staticmethod
+    def validate_date_fields(start_date, finish_date):
+        if start_date is not None or finish_date is not None:
+            if start_date > finish_date:
+                raise ValidationError(
+                    "Finish date cannot be earlier than the start date."
+                )
+
+    def clean(self):
+        Task.validate_date_fields(self.start_date, self.finish_date)
