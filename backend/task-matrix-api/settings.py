8f7ac06ea58f1073e340b04817be41aa548d6176
Tetiana Kozakova <117391668+kleotan901@users.noreply.github.com>
2024-09-05 21:06:35 +0300

    Update settings.py

--- backend/task-matrix-api/settings.py
+++ backend/task-matrix-api/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = "staticfiles/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # Default primary key field type
