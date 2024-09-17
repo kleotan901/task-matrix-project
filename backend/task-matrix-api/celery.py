7fc1f6d77743fc691eaaefb3f5a9d289ae9c959f
Tetiana Kozakova <kleotan901@gmail.com>
2024-09-17 13:37:04 +0300

    Updated celery configurations for deployment on AWS

--- backend/task-matrix-api/celery.py
+++ backend/task-matrix-api/celery.py
@@ -1,7 +1,22 @@
-import os
 from celery import Celery
+from django.conf import settings
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "task-matrix-api.settings")
-app = Celery("task-matrix-api")
-app.config_from_object("django.conf:settings", namespace="CELERY")
-app.autodiscover_tasks()
+sqs_queue_url = settings.SQS_QUEUE_URL
+aws_access_key = settings.AWS_ACCESS_KEY
+aws_secret_key = settings.AWS_SECRET_KEY
+
+app = Celery(
+    "app",
+    broker_url=f"sqs://{aws_access_key}:{aws_secret_key}@",
+    broker_transport_options={
+        "region": "us-east-1", # your AWS SQS region
+        "predefined_queues": {
+            "celery": {  ## the name of the SQS queue
+                "url": sqs_queue_url,
+                "access_key_id": aws_access_key,
+                "secret_access_key": aws_secret_key,
+            }
+        },
+    },
+    task_create_missing_queues=False,
+)
